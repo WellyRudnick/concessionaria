9f382f187071ad1c3e20e846694e3ea708d4f17e
ga6rielsilva <ga6rielsilva@gmail.com>
2024-10-18 16:25:12 -0300

    18

--- Cadastro_vaiculos/main.py
+++ Cadastro_vaiculos/main.py
@@ -7,3 +7,11 @@ class Veiculo():
 
     def __str__(self):
         return f'{self.marca} {self.modelo} {self.ano} {self.cor}'
+
+class Carro(Veiculo):
+    def __init__(self, marca, modelo, ano, cor, portas):
+        super().__init__(marca, modelo, ano, cor)
+        self.portas = portas
+
+    def __str__(self):
+        return f'{super().__str__()} {self.portas} portas'
