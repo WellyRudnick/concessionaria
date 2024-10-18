69db6c5a19930073c50c3019ae8660887949c642
ga6rielsilva <ga6rielsilva@gmail.com>
2024-10-18 16:33:01 -0300

    Atualização

--- Cadastro_vaiculos/main.py
+++ Cadastro_vaiculos/main.py
@@ -1,17 +1,7 @@
 class Veiculo():
-    def __init__(self, marca, modelo, ano, cor):
-        self.marca = marca
+
+    def __init__(self, modelo, marca, ano, valor_mercado):
         self.modelo = modelo
+        self.marca = marca
         self.ano = ano
-        self.cor = cor
-
-    def __str__(self):
-        return f'{self.marca} {self.modelo} {self.ano} {self.cor}'
-
-class Carro(Veiculo):
-    def __init__(self, marca, modelo, ano, cor, portas):
-        super().__init__(marca, modelo, ano, cor)
-        self.portas = portas
-
-    def __str__(self):
-        return f'{super().__str__()} {self.portas} portas'
+        self.valor_mercado = valor_mercado
