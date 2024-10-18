70166a1780209b061ae8bbc3da71acb73e40c7a9
ga6rielsilva <ga6rielsilva@gmail.com>
2024-10-18 16:39:14 -0300

    18

--- Cadastro_vaiculos/main.py
+++ Cadastro_vaiculos/main.py
@@ -5,3 +5,16 @@ class Veiculo():
         self.marca = marca
         self.ano = ano
         self.valor_mercado = valor_mercado
+
+    # Métodos responsáveis por retornar os atributos dos veículos
+    def getMarca(self):
+        return self.marca
+    
+    def getModelo(self):
+        return self.modelo
+
+    def getAno(self):
+        return self.ano
+    
+    def getValor(self):
+        return self.valor_mercado
