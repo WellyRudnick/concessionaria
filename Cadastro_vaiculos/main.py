0bb72ebc5b14691184d1fa9d01a9942543be0276
Welly <wellysonrudnick13@gmail.com>
2024-10-18 16:19:34 -0300

    test

--- Cadastro_vaiculos/main.py
+++ Cadastro_vaiculos/main.py
@@ -1,1 +1,9 @@
 class Veiculo():
+    def __init__(self, marca, modelo, ano, cor):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.cor = cor
+
+    def __str__(self):
+        return f'{self.marca} {self.modelo} {self.ano} {self.cor}'
